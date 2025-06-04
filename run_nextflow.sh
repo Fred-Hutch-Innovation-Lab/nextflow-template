05bdb03f845cfdde4469bfddf31b7bd6216c579a
derrik-gratz <derrik.gratz@gmail.com>
2025-06-04 12:53:16 -0700

    comments

--- run_nextflow.sh
+++ run_nextflow.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 module load Apptainer Nextflow
+# Needed to allow apptainer to access various directories accessed by programs such as 
+# mixcr that writes to /loc/scratch
 export NXF_APPTAINER_HOME_MOUNT=true;
 nextflow run . -c run_arguments.config
